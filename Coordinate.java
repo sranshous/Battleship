dbef2fad91669b831bb5b5dc24faa16bf0fd07c7
Stephen Ranshous <sranshous@gmail.com>
2013-01-22 16:25:47 -0500

    An easier way to transport coordinate data Instead of passing arrays or something to represent row,col information we will use a Coordinate

---
+++ Coordinate.java
@@ -0,0 +1,17 @@
+/**
+ * An easier way to transport coordinate data
+ */
+public class Coordinate {
+    public int row;
+    public int col;
+
+    public Coordinate() {
+        this.row = -1;
+        this.col = -1;
+    }
+
+    public Coordinate(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
