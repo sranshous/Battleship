e076fa60e99abd864025e63363a1683b99d62da5
Stephen Ranshous <sranshous@gmail.com>
2013-01-22 19:46:59 -0500

    This restructures the board classes Added a new abstract class called Board. It is the superclass of both PlayerBoard and Radar. This was done for polymorphism purposes (e.g. the displayBoard method)

--- Radar.java
+++ Radar.java
@@ -9,10 +9,7 @@ import java.util.Arrays;
  * O represents a miss
  * * represents an unused space
  */
-public class Radar {
-    private char[][] radar;
-    private int radarWidth;
-    private int radarHeight;
+public class Radar extends Board {
 
     public Radar() {
         this(10, 10);
@@ -20,23 +17,23 @@ public class Radar {
 
     /* If we want a 10x10 board we create an 11x11 array so that we can omit
      * row and column zero and just use 1:n and 1:m indicies */
-    public Radar(int radarHeight, int radarWidth) {
-        this.radarWidth = radarHeight+1;
-        this.radarHeight = radarWidth+1;
-        this.radar = new char[this.radarHeight][this.radarWidth];
-        radarInit();
+    public Radar(int boardHeight, int boardWidth) {
+        this.boardWidth = boardHeight+1;
+        this.boardHeight = boardWidth+1;
+        this.board = new char[this.boardHeight][this.boardWidth];
+        boardInit();
     }
 
-    private void radarInit() {
+    protected void boardInit() {
         /* clear the zero'th row and column */
-        for(int i = 0; i < this.radarHeight; i++)
-            this.radar[i][0] = ' ';
-        for(int i = 0; i < this.radarWidth; i++)
-            this.radar[0][i] = ' ';
+        for(int i = 0; i < this.boardHeight; i++)
+            this.board[i][0] = ' ';
+        for(int i = 0; i < this.boardWidth; i++)
+            this.board[0][i] = ' ';
 
-        for(int i = 1; i < this.radarHeight; i++) {
-            for(int j = 1; j < this.radarWidth; j++) {
-                this.radar[i][j] = '*';
+        for(int i = 1; i < this.boardHeight; i++) {
+            for(int j = 1; j < this.boardWidth; j++) {
+                this.board[i][j] = '*';
             }
         }
     }
@@ -54,11 +51,11 @@ public class Radar {
             return false;
 
         if(hitOrMiss == 'h' || hitOrMiss == 'H') {
-            this.radar[row][col] = 'X';
+            this.board[row][col] = 'X';
             return true;
         }
         else { // no need to check for 'm' or 'M' since we passed the check params
-            this.radar[row][col] = 'O';
+            this.board[row][col] = 'O';
             return true;
         }
     }
@@ -66,12 +63,12 @@ public class Radar {
     /* returns true if it is a valid location, false otherwise */
     private boolean checkLocation(int row, int col) {
         /* check if the coordinates are in the playable range */
-        if(row < 1 || row > this.radarHeight-1) {
-            System.err.println(String.format("Error: The row must be between 1 and %d", this.radarHeight-1));
+        if(row < 1 || row > this.boardHeight-1) {
+            System.err.println(String.format("Error: The row must be between 1 and %d", this.boardHeight-1));
             return false;
         }
-        else if(col < 1 || col > this.radarWidth-1) {
-            System.err.println(String.format("Error: The column must be between 1 and %d", this.radarWidth-1));
+        else if(col < 1 || col > this.boardWidth-1) {
+            System.err.println(String.format("Error: The column must be between 1 and %d", this.boardWidth-1));
             return false;
         }
 
@@ -105,25 +102,25 @@ public class Radar {
         if(!checkLocation(row, col))
             return '\0';
 
-        return this.radar[row][col];
+        return this.board[row][col];
     }
 
-    public char[][] getRadar() {
-        return this.radar;
+    public char[][] getBoard() {
+        return this.board;
     }
 
     public int getRadarWidth() {
-        return this.radarWidth;
+        return this.boardWidth;
     }
 
     public int getRadarHeight() {
-        return this.radarHeight;
+        return this.boardHeight;
     }
 
     public String toString() {
         StringBuilder s = new StringBuilder();
-        for(int i = 0; i < this.radarHeight; i++) {
-            s.append(Arrays.toString(this.radar[i]));
+        for(int i = 0; i < this.boardHeight; i++) {
+            s.append(Arrays.toString(this.board[i]));
             s.append("\n");
         }
 
